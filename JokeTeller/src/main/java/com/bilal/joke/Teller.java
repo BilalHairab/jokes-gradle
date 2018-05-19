51cfd9263ecc014584039e865fdcbb08d85105ee
Bilal Ibrahim Hairab <bilal.hairab@gmail.com>
2018-05-19 21:07:49 +0200

    Step 3.2: Fetch a joke and display it (fix formatting jokes issue)

--- JokeTeller/src/main/java/com/bilal/joke/Teller.java
+++ JokeTeller/src/main/java/com/bilal/joke/Teller.java
@@ -3,10 +3,10 @@ package com.bilal.joke;
 import java.util.Random;
 
 public class Teller {
-    static String[] jokes = {"Don’t interrupt someone working intently on a puzzle. Chances are, you’ll hear some crosswords.",
-    "Q. What’s the difference between ignorance and apathy? A. I don’t know and I don’t care.",
-    "I’m a big fan of whiteboards. I find them quite re-markable.",
-    "Did you see the movie about the hot dog? It was an Oscar wiener.",
+    private static String[] jokes = {"Don\'t interrupt someone working intently on a puzzle.\nChances are, you\'ll hear some crosswords.",
+    "Q. What\'s the difference between ignorance and apathy?\nA. I don\'t know and I don\'t care.",
+    "I\'m a big fan of whiteboards.\nI find them quite re-markable.",
+    "Did you see the movie about the hot dog?\nIt was an Oscar wiener.",
     "The shovel was a ground-breaking invention."};
     public static String getJoke(){
         Random rand = new Random();
