f21cdc778837228fc8316aafebe1b3f92644eb22
Bilal Ibrahim Hairab <bilal.hairab@gmail.com>
2018-05-21 15:13:20 +0200

    step 7: add progress bar (Optional)

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -2,45 +2,36 @@ package com.udacity.gradle.builditbigger;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.ProgressBar;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements EndpointsAsyncTask.GetJokeTaskListener {
+    private ProgressBar spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        spinner = findViewById(R.id.progressBar);
     }
 
     public void tellJoke(View view) {
-        new EndpointsAsyncTask().execute(this);
+        spinner.setVisibility(View.VISIBLE);
+        EndpointsAsyncTask jokeTask = new EndpointsAsyncTask();
+        jokeTask.setListener(this);
+        jokeTask.execute(this);
     }
 
 
+    @Override
+    public void onComplete(String joke, Exception e) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                spinner.setVisibility(View.INVISIBLE);
+            }
+        });
+
+    }
 }
